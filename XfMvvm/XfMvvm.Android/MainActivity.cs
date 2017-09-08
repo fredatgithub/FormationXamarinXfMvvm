5929280af8a78bd43dfc3d2b0bd95550b3823608
Fred <domaine75@gmail.com>
2017-09-08 08:57:22 +0100

    resharper code

--- XfMvvm/XfMvvm.Android/MainActivity.cs
+++ XfMvvm/XfMvvm.Android/MainActivity.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace XfMvvm.Droid
@@ -24,4 +20,3 @@ namespace XfMvvm.Droid
     }
   }
 }
-
